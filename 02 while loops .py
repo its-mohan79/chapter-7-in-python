cd6faaeba1edba29a3f3616176cf1e156a509753
its-mohan79 <mohanroy8989@gmail.com>
2026-01-05 14:57:58 +0530

    complete the lesson 2

---
+++ 02 while loops .py
@@ -0,0 +1,35 @@
+
+#quiz number 1
+
+i = 1
+
+while (i < 6):
+    print(i)
+    i += 1  #i=i+1
+
+
+'''
+output:
+1
+2
+3
+4
+5
+'''
+
+
+#quiz number 2
+i1=0
+while i1<5:
+    print("mohan")
+    i1=i1+1
+
+
+
+
+#quiz number 3   
+l=[1, "mohan",False,"this","rohan","shubham","ankit"]
+i2=0
+while(i2<len(l)):
+    print(l[i2])
+    i2=i2+1
