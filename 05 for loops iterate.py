7c8ca1ad177b0bd85fff4b932886928063c71967
its-mohan79 <mohanroy8989@gmail.com>
2026-01-05 15:12:47 +0530

    complete the chapter 5

---
+++ 05 for loops iterate.py
@@ -0,0 +1,22 @@
+
+#for loops in list
+
+l=[1,4,6,234,6,764]
+
+for i in l:
+    print(i)
+
+
+#for loops in tuple
+
+t=(2,4,6,234,6,764)
+for i1 in t:
+    print(i1)    
+
+
+#example for loops with string
+
+s="mohan"
+for i3 in s:
+    print(i3)
+
